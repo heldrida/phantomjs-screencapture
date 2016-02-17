fc5d3bd17e084600318ab7e1f9e8144121b3cce1
Helder <helder@lamoulade.com>
2016-02-17 19:55:48 +0000

    set useragent and viewport size;

--- screencapture.js
+++ screencapture.js
@@ -7,6 +7,11 @@ var fs = require('fs');
 
 // the webpage module
 var page = require('webpage').create();
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
+page.viewportSize = {
+  width: 1025,
+  height: 768
+};
 
 // properties
 var path = '../../louboutin/pages/';
